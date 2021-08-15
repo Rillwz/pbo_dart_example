8877453a8b21b2649bf68d458cdd3997241924b5
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-16 00:26:35 +0700

    MonsterUburUbur

--- bin/Inheritance/main.dart
+++ bin/Inheritance/main.dart
@@ -1,17 +1,33 @@
 import 'hero.dart';
 import 'monster.dart';
+import 'monster_ubu_ubur.dart';
+import 'monster_kecoak.dart';
 
 dynamic main(List<String> arguments) async {
   Hero h = Hero();
-  Monster m = Monster();
+  Monster monster = MonsterUburUbur();
+  MonsterUburUbur u = MonsterUburUbur();
+
+  List<Monster> monsters = [];
+
+  monsters.add(MonsterUburUbur());
+  monsters.add(MonsterKecoak());
+  monsters.add(MonsterUburUbur());
+
+  print((monster as MonsterUburUbur).swim());
+
+  for (Monster m in monsters) {
+    if (m is MonsterUburUbur) {
+      print(m.eatHuman());
+    }
+  }
 
   h.hp = 10;
-  m.hp = 10;
+  u.hp = 3;
 
   print('hero hp:' + h.hp.toString());
-  print('monster hp:' + m.hp.toString());
+  print('monster hp:' + u.hp.toString());
 
   print(h.hitMonster());
-  print(m.eatHuman());
+  print(u.swim());
 }
-  
